d21bbf764d656b9caaeb3f1816db359ed8ae83aa
Yuri Borunov <yury@borunov.com>
2015-04-14 13:43:22 -0400

    changes firebase connection point

--- www/js/services.js
+++ www/js/services.js
@@ -3,9 +3,9 @@ angular.module('starter.services', [])
  * A simple example service that returns some data.
  */
 .factory('fireBaseData', function($firebase) {
-  var ref = new Firebase("https://luminous-fire-3429.firebaseio.com/"),
-      refExpenses = new Firebase("https://luminous-fire-3429.firebaseio.com/expenses"),
-      refRoomMates = new Firebase("https://luminous-fire-3429.firebaseio.com/room-mates");
+  var ref = new Firebase("https://scorching-torch-9009.firebaseIO.com/"),
+      refExpenses = new Firebase("https://scorching-torch-9009.firebaseIO.com/expenses"),
+      refRoomMates = new Firebase("https://scorching-torch-9009.firebaseIO.com/room-mates");
   return {
     ref: function () {
       return ref;
