77405c0365407f0ba38cb9a9e9701061cbbc6e7f
Yuri Borunov <yury@borunov.com>
2015-04-17 13:22:15 -0400

    added friends, users and expenses services

--- www/js/services.js
+++ www/js/services.js
@@ -4,17 +4,71 @@ angular.module('starter.services', [])
  */
 .factory('fireBaseData', function($firebase) {
   var ref = new Firebase("https://scorching-torch-9009.firebaseIO.com/"),
-      refExpenses = new Firebase("https://scorching-torch-9009.firebaseIO.com/expenses"),
-      refRoomMates = new Firebase("https://scorching-torch-9009.firebaseIO.com/room-mates");
-  return {
+      refExpenses,
+      refUsers,
+      refFriends,
+      self = {};
+  self = {
     ref: function () {
       return ref;
     },
     refExpenses: function () {
       return refExpenses;
     },
-    refRoomMates: function () {
-      return refRoomMates;
+    refFriends: function () {
+      return refFriends;
+    },
+    refUsers: function () {
+      return refUsers;
+    },
+    refInit: function () {
+      refExpenses = new Firebase("https://scorching-torch-9009.firebaseIO.com/expenses"),
+      refUsers = new Firebase("https://scorching-torch-9009.firebaseIO.com/users"),
+      refFriends = new Firebase("https://scorching-torch-9009.firebaseIO.com/friends");
     }
-  }
+  };
+  self.refInit();
+  return self;
+})
+.service('friends', function ($firebaseArray, fireBaseData) {
+  var self = {};
+  
+  self.list = $firebaseArray(fireBaseData.refFriends());
+  
+  self.ready = function (cb) {
+    self.list.$loaded().then(function () {
+      cb();
+    });
+  };
+
+  self.getFriends = function (of) {
+    var result = [];
+    self.list.forEach(function (item) {
+      if (item.user_email === of) {
+        result.push(item.friend);
+      }
+    });
+    return result;
+  };
+  
+  return self;
+})
+.service('users', function ($firebaseArray, fireBaseData) {
+  var self = {};
+  self.list = $firebaseArray(fireBaseData.refUsers());
+  self.getByEmail = function (email) {
+    var result = false;
+    self.list.forEach(function (item) {
+      if (item.email === email) {
+        result = item;
+      }
+    });
+    return result;
+  };
+  return self;
+})
+.service('expenses', function ($firebaseArray, fireBaseData) {
+  var self = {};
+  self.list = $firebaseArray(fireBaseData.refExpenses());
+  return self;
 });
